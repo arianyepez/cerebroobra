b2d6b3381b65e1e3fe0f02f060311a4f38209093
arianyepez <yepezadrian753@gmail.com>
2026-04-05 12:20:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ with c1:
             st.write(f"*Tubos Estructurales {medida_tubo}:* {total_tubos} piezas")
 
     with st.expander("🧱 Paredes y Acabados"):
-        st.write(f"**Bloques de 15c
+st.write(f"**Bloques de 15cm:**{bloques_total}unidades")
